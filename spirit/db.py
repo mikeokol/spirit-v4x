773b52f07c817814cd15753d343760cbdd7e4bd8
mikeokol <125mikeokol@gmail.com>
2026-01-18 19:44:51 -0500

    db.py

---
+++ spirit/db.py
@@ -0,0 +1,17 @@
+import aiosqlite
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
+from spirit.config import settings
+
+engine = create_async_engine(
+    settings.db_url,
+    echo=False,
+    connect_args={"check_same_thread": False} if "sqlite" in settings.db_url else {},
+)
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
