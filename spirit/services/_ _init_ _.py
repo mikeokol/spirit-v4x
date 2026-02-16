e0af45ba1b9173845bb785cc22795556a32e0f28
mikeokol <125mikeokol@gmail.com>
2026-02-16 03:28:14 -0500

    Update _ _init_ _.py

--- spirit/services/_ _init_ _.py
+++ spirit/services/_ _init_ _.py
@@ -5,5 +5,15 @@ Spirit services package.
 from spirit.services.privacy import PrivacyFilter
 from spirit.services.enrichment import ContextEnricher
 from spirit.services.jitai import JITAIEngine
+from spirit.services.causal_inference import CausalInferenceEngine, ExperimentDesigner
+from spirit.services.goal_integration import BehavioralGoalBridge, TrajectoryBehavioralAnalyzer
 
-__all__ = ["PrivacyFilter", "ContextEnricher", "JITAIEngine"]
+__all__ = [
+    "PrivacyFilter",
+    "ContextEnricher",
+    "JITAIEngine",
+    "CausalInferenceEngine",
+    "ExperimentDesigner",
+    "BehavioralGoalBridge",
+    "TrajectoryBehavioralAnalyzer"
+]
