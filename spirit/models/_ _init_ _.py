db1bd1e7e13bb21c354e6e7485ce077f7ed71f01
mikeokol <125mikeokol@gmail.com>
2026-02-28 01:35:25 -0500

    Update _ _init_ _.py

--- spirit/models/_ _init_ _.py
+++ spirit/models/_ _init_ _.py
@@ -12,12 +12,28 @@ from spirit.models.behavioral import (
     AppCategory
 )
 
+# Digital Twin Models (v2.3) - In-silico experimentation
+from spirit.models.mechanisms import MechanismActivation
+from spirit.models.mechanistic_user_model import (
+    UserState,
+    InterventionDose,
+    SimulationResult,
+    MechanisticUserModel
+)
+
 __all__ = [
+    # Behavioral observation models
     "BehavioralObservation",
     "ScreenTimeSession", 
     "EMARequest",
     "UserCausalHypothesis",
     "ObservationType",
     "PrivacyLevel",
-    "AppCategory"
+    "AppCategory",
+    # Digital Twin simulation models (v2.3)
+    "MechanismActivation",
+    "UserState",
+    "InterventionDose",
+    "SimulationResult",
+    "MechanisticUserModel",
 ]
